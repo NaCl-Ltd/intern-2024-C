a9a564d3030f16830be7449611b51806966d6d0e
Junwoo Cheon <jwcheonx@gmail.com>
2024-09-06 11:54:58 +0900

    Refactor `logged_in_user` method to `require_user`

--- app/controllers/microposts_controller.rb
+++ app/controllers/microposts_controller.rb
@@ -1,5 +1,5 @@
 class MicropostsController < ApplicationController
-  before_action :logged_in_user, only: %i[create destroy news toggle_pinned]
+  before_action :require_user
   before_action :correct_user, only: %i[destroy toggle_pinned]
 
   def create
