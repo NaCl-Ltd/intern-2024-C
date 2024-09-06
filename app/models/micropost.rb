ca1c100ae6aedfd2e0883a2264c4d530326de71c
j2112karinohinata <j2112@matsue-ct.ac.jp>
2024-09-06 13:39:01 +0900

    ひとつの投稿に複数の画像をアップロード可能にする

--- app/models/micropost.rb
+++ app/models/micropost.rb
@@ -1,6 +1,6 @@
 class Micropost < ApplicationRecord
   belongs_to :user
-  has_many_attached :image do |attachable|
+  has_many_attached :images do |attachable|
     attachable.variant :display, resize_to_limit: [200, 200]
   end
 
@@ -8,14 +8,21 @@ class Micropost < ApplicationRecord
 
   validates :user_id, presence: true
   validates :content, presence: true, length: { maximum: 140 }
-  validates :image,   content_type: { in: %w[image/jpeg image/gif image/png],
+  validates :images, content_type: { in: %w[image/jpeg image/gif image/png],
                                       message: "must be a valid image format" },
                       size:         { less_than: 5.megabytes,
                                       message:   "should be less than 5MB" }
+  validate :images_limit
 
   after_save :ensure_single_pinned_post, if: -> { pinned? }
 
   private
+
+  def images_limit
+    if images.size > 4
+      errors.add(:images, "cannot post more than 4")
+    end
+  end
 
   def ensure_single_pinned_post
     user.microposts.where(pinned: true).where.not(id: id).update_all(pinned: false)
